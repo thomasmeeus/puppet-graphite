80af52582c47eb58feaf60d1f4e3aedcdd59bcbc
Jurgen De Backer <jurgen.debacker@cegeka.be>
2012-12-17 14:35:31 +0100

    package python-txamqp is now included (required for carbon-rabbitmq integration)

--- manifests/carbon/package.pp
+++ manifests/carbon/package.pp
@@ -2,4 +2,15 @@ class graphite::carbon::package {
   package {'carbon':
     ensure => present;
   }
+
+  $amqp_enable_local = $graphite::amqp_enable
+
+  if ($amqp_enable_local) {
+    # Python txamqp (required for carbon/AMQP integration); edit /etc/carbon.conf to enable AMQP support in carbon;
+    # (Patched version)
+    package { 'python-txamqp':
+      ensure => '0.3-2.cegeka',
+    }
+  }
 }
+
