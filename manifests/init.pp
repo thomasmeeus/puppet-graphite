70fbe0956dc9054a2c99143a23f80219e0ad0543
Kristof Willaert <kristof.willaert@cegeka.be>
2013-12-17 14:22:39 +0100

    Cleanup main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,24 +4,20 @@
 #
 class graphite (
   $datadir                  = '/var/lib/carbon',
-  $timezone     = $::timezone,
-  $amqp_enable   = false,
-  $amqp_verbose  = 'False',
-  $amqp_host     = 'localhost',
-  $amqp_port     = '5672',
-  $amqp_vhost    = '/',
-  $amqp_user     = 'guest',
-  $amqp_password = 'guest',
-  $amqp_exchange = 'graphite',
+  $timezone                 = $::timezone,
+  $amqp_enable              = false,
+  $amqp_verbose             = 'False',
+  $amqp_host                = 'localhost',
+  $amqp_port                = '5672',
+  $amqp_vhost               = '/',
+  $amqp_user                = 'guest',
+  $amqp_password            = 'guest',
+  $amqp_exchange            = 'graphite',
   $amqp_metric_name_in_body = 'False'
-  )
-  {
-
-  include graphite::carbon
-  class { 'graphite::web':
-    timezone => $timezone,
-  }
+)
+{
+  class { 'graphite::carbon': datadir => $datadir }
+  class { 'graphite::web': timezone => $timezone, datadir => $datadir }
 
   Class['graphite::carbon'] -> Class['graphite::web']
-
 }
