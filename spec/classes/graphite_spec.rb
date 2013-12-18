5eb0fd15333cf0f846cbc843534653c334d65774
Kristof Willaert <kristof.willaert@cegeka.be>
2013-12-18 10:42:22 +0100

    Tests for datadir parameter

--- spec/classes/graphite_spec.rb
+++ spec/classes/graphite_spec.rb
@@ -10,14 +10,37 @@ describe 'graphite' do
   context 'with default parameters' do
     let (:params) { {} }
 
+<<<<<<< HEAD
+    it { should contain_class('graphite::carbon') }
+    it { should contain_class('graphite::web').with(
+      {
+        :timezone => 'CET',
+        :datadir  => '/var/lib/carbon'
+      }
+    )}
+=======
     it { should include_class('graphite::carbon') }
     it { should contain_class('graphite::web').with_timezone('CET') }
+>>>>>>> dd600f8c6ff6077389024da0af0f154ae113ef16
   end
 
-  context 'with timezone => Europe/Brussels' do
-    let (:params) { { :timezone => 'Europe/Brussels' } }
+  context 'with timezone => Europe/Brussels and datadir => /data/carbon' do
+    let (:params) { {
+      :timezone => 'Europe/Brussels',
+      :datadir  => '/data/carbon'
+    } }
 
+<<<<<<< HEAD
+    it { should contain_class('graphite::carbon') }
+    it { should contain_class('graphite::web').with(
+      {
+        :timezone => 'Europe/Brussels',
+        :datadir  => '/data/carbon'
+      }
+    )}
+=======
     it { should include_class('graphite::carbon') }
     it { should contain_class('graphite::web').with_timezone('Europe/Brussels') }
+>>>>>>> dd600f8c6ff6077389024da0af0f154ae113ef16
   end
 end
